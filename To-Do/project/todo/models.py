da7861d792173447396a5e43a8260e86af70bc45
Ronit Sharma <100018653+RonitSharma705@users.noreply.github.com>
2025-11-14 11:47:49 +0530

    main

--- To-Do/project/todo/models.py
+++ To-Do/project/todo/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Todo(models.Model):
     Title=models.CharField(max_length=50)
     Description=models.TextField(max_length=100)
--- To-Do/project/todo/views.py
+++ To-Do/project/todo/views.py
@@ -4,8 +4,6 @@ from .models import Todo
 from .serializers import TodoSerializers
 
 
-# Create your views here.
-
 class TodoViews(viewsets.ModelViewSet):
     queryset= Todo.objects.all().order_by('-Created_at')
     serializer_class = TodoSerializers
