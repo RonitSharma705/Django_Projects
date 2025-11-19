1b8dfa5c71761d18b0b7fa2221fdd23854652daf
Ronit Sharma <100018653+RonitSharma705@users.noreply.github.com>
2025-11-19 20:49:45 +0530

    main

--- Auth System/auth_system/accounts/views.py
+++ Auth System/auth_system/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SignupSerializers
 
-# Create your views here.
+class SignupView(APIView):
+    def post(self,request):
+        serializer =SignupSerializers(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'Message':"User Created SuccessFully"},status=201)
+        return Response(serializer.errors,status=400)
